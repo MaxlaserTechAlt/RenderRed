e2b3e90b8a8c7f728fba15d584de55d8188bd763
MaxlaserTechAlt <135626624+MaxlaserTechAlt@users.noreply.github.com>
2024-03-17 06:56:31 +0700

    Update System.lua

--- Core/System.lua
+++ Core/System.lua
@@ -1,21 +1,27 @@
 local System = {}
 local lplr = game.Players.LocalPlayer
-writefile("FirstTime", "nil")
+if not isfile("FirstTime.json") then
+    writefile("FirstTime.json", "nil")
+end
 System.Check(str)
     return readfile(str)
 end
 System.CreateKey(key)
     writefile("key-".. key)
     if not isfolder("vape/Render") then
-        lplr:Kick("Failed To Create key")
+        lplr:Kick("Failed To Create key. Please Go To @.gg/render or Render Red Server!")
     else
         writefile("vape/Render/key-"..key)
     end
 end
 System.ReadFile(name)
     if System.Check("FirstTime") == nil then
+        writefile(name.. ".rr", "true")
         delfile("FirstTime.json")
         writefile("FirstTime.json", "false")
+    end
+    if not isfile(name..".rr") then
+        lplr:Kick("Something When Wrong...")
     end
 end
 return System
