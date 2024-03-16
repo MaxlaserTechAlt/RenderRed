5d060ce437e7b972dd9bb7cc3793559fcec841d9
MaxlaserTechAlt <135626624+MaxlaserTechAlt@users.noreply.github.com>
2024-03-17 06:52:18 +0700

    specialsploit

--- Core/System.lua
+++ Core/System.lua
@@ -1,11 +1,21 @@
 local System = {}
 local lplr = game.Players.LocalPlayer
+writefile("FirstTime", "nil")
+System.Check(str)
+    return readfile(str)
+end
 System.CreateKey(key)
     writefile("key-".. key)
     if not isfolder("vape/Render") then
-        lplr:Kick("Failed To Create key. Please Go To @.gg/render")
+        lplr:Kick("Failed To Create key")
     else
         writefile("vape/Render/key-"..key)
+    end
+end
+System.ReadFile(name)
+    if System.Check("FirstTime") == nil then
+        delfile("FirstTime.json")
+        writefile("FirstTime.json", "false")
     end
 end
 return System
